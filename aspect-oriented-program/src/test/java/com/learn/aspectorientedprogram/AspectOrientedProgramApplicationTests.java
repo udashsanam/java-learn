0da2479860bb41b7490d2ecc5d69aafda10ae4f3
udashsanam <udashsanam@gmail.com>
2023-01-04 11:53:20 +0545

    add: aspect oriented programming learn

---
+++ aspect-oriented-program/src/test/java/com/learn/aspectorientedprogram/AspectOrientedProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.aspectorientedprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspectOrientedProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
