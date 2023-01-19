5e59f25b2d96d89274b6321a8339de439b24f5ca
udashsanam <udashsanam@gmail.com>
2023-01-19 11:30:16 +0545

    add: api to send message

---
+++ messaging-rabbitmq/src/main/java/com/learn/messagingrabbitmq/util/RabbitMqUtil.java
@@ -0,0 +1,9 @@
+package com.learn.messagingrabbitmq.util;
+
+public interface RabbitMqUtil {
+
+    static final String TOPIC_EXCHANGE_NAME = "spring-boot-exchange";
+
+    static final String QUEUE_NAME = "spring-boot";
+
+}
