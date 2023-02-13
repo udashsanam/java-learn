c9c6c75ad5cc8025041af3ea59bd4f1eff4e46eb
udashsanam <udashsanam@gmail.com>
2023-02-13 12:23:34 +0545

    add: web rtc

--- web-rtc/src/main/java/com/learn/webrtc/config/WebSocketConfiguration.java
+++ web-rtc/src/main/java/com/learn/webrtc/config/WebSocketConfiguration.java
@@ -1,2 +1,17 @@
-package com.learn.webrtc.config;public class WebSocketConfiguration {
+package com.learn.webrtc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfiguration implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new SocketHandler(),"/socket")
+                .setAllowedOrigins("*");
+    }
 }
