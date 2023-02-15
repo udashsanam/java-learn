929f0b1299b3bb42741089a0428f8a688b4d8605
udashsanam <udashsanam@gmail.com>
2023-02-15 09:28:50 +0545

    add: micro service

---
+++ multi-language/src/test/java/com/learn/multilanguage/MultiLanguageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.multilanguage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiLanguageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
